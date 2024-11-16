dee864e4f13fd51a0eb6a5a91fffa9b9b015a594
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-11-16 15:36:52 +0100

    navbar & hero section completed

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Hero.css';
+import profile_img from '../../assets/profile_img.jpg';
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero">
+        <img src={profile_img} alt="" />
+        <div className="content">
+          <h6 className="greeting"> Hi, My Name is</h6>
+          <h1 className="main_head">
+            <span className="highlight">👋🏽 Tosin Faith O.</span> <br />
+            <span className="rest">A Frontend Dev, Based in Lagos</span>
+          </h1>
+          <h6 className="subtitle">
+          I build websites with a focus on responsive design, accessibility and aesthetics. When I'm not working, I share my knowledge by teaching others, writing articles and speaking at events. (don't judge me, I love work 😩😩).
+            </h6>
+            <div className="social-icons">
+              <a href="https://wa.me/+2348123539192" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-whatsapp"></i>
+            </a>
+              <a href="https://facebook.com/thetosinfaith" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="https://instagram.com/thetosinfaith" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a href="https://github.com/thetosinfaith" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="https://linkedin.com/in/thetosinfaith" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a href="https://twitter.com/thetosinfaith" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="mailto:tosinfaith.contact@gmail.com" target="_blank" rel="noopener noreferrer">
+                <i className="fas fa-envelope"></i>
+              </a>
+              <a href="https://linktr.ee/thetosinfaith" target="_blank" rel="noopener noreferrer">
+                <i className="fas fa-link"></i>
+              </a>
+            </div>
+          <a href="mailto:tosinfaith.contact@gmail.com" className="cta" target="_blank" rel="noopener noreferrer">
+            <button className="hero_button">Available for Work</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={logo} alt="" />
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+      </ul>
+      <div className="nav-connect">Download Resume</div>
+    </div>
+  )
+}
+
+export default Navbar
