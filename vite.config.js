1317a6958fb1fc4a0a6604a085d1d42fd694343d
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2025-02-14 22:02:33 +0100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, 
-  },
+  base: "/", 
   build: {
-    outDir: 'dist', 
+    outDir: "dist", 
+  },
+  server: {
+    fs: {
+      strict: false, 
+    },
   },
 });
